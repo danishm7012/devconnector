2be08954b61180317053bcc48cdc5c49e87999d8
Danish Mehmood <danishm7012@gmail.com>
2020-12-12 18:42:14 +0500

    push into git

--- server.js
+++ server.js
@@ -20,7 +20,12 @@ const db = require('./config/keys').mongoURI
 
 // Connect to MongoDB
 mongoose
-  .connect(db)
+  .connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
   .then(() => console.log('MongoDB Connected'))
   .catch((err) => console.log(err))
 
